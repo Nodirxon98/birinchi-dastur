95c0491f1b7461ce714c247bd52c28b8c3e7d570
Nodirxon <nodirxonqurbonov99@gmail.com>
2023-04-11 16:39:32 +0500

    salom github

---
+++ app.py
@@ -0,0 +1,6 @@
+# from main import Function
+# lst = [23,43,4,5,6,8,7,34,22,444]
+# st = {23,43,4,5,6,8,7,34,22,444}
+
+# Function(lst)
+# Function(st)
---
+++ main.py
@@ -0,0 +1,50 @@
+
+# def Function(son):
+#     for x in son:
+#         if x > 7:
+#             print(x)
+
+# def lmb(a,b):
+#     l = lambda x,y : x*y
+#     print(l(a,b))
+# lmb(3,4)
+
+# def lmb():
+#     return lambda x,y : x*y~
+# l = lmb()
+# print(l(2,5))
+# ===============================================
+
+# oop
+# object orinted pragramming - obektga yo'naltirilgan dasturlash 
+#n class
+
+# ism = input("ismingizni kiriting: ")
+# familya = input("familyangizn kiriting: ")
+# yosh = int(input("yoshingizni kiriting: "))
+# class Person:
+#     def __init__(self, name, firstName, age):
+#         self.name = name 
+#         self.firstName = firstName
+#         self.age = age
+
+#     def nameClick(self):
+#         print(self.name)
+
+#     def firstNameClick(self):
+#         print(self.firstName)
+
+#     def ageClick(self):
+#         print(self.age)
+
+#     def fulName(self):
+#         print(f"salom {self.name} {self.firstName} saytimizga hush kelibsiz")
+    
+#     def yoshTekshir(self):
+#         if self.age >= 20:
+#             print("hush kelibsiz"+ ' ' + self.name)
+#         else:
+#             print("siz yosh ekansiz"+ ' ' + self.name)
+# newClass = Person(ism,familya,yosh)
+# newClass.yoshTekshir()
+# ==================================================================================
